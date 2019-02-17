9f6e88b797763576a591d7a3e08af090f5ef60e3
Sven Kirschbaum <sven@elite12.de>
2019-02-17 21:49:38 +0100

    Fixed Listener issue

--- Musikbot Chrome Extension/scripts/background.js
+++ Musikbot Chrome Extension/scripts/background.js
@@ -1,33 +1,34 @@
 const regex = /^(http(s)??\:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/;
+
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.storage.sync.set({
 		authtoken : ""
 	}, function() {
 
 	});
+});
 
-	// Clicking on the button will give the extension access to the current tab,
-	// so the icon will not be disabled after the condition below no longer
-	// applies.
-	// Therefore we have to switch to a more powerful approach to manage the
-	// extension state
-	/*
-	 * chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	 * chrome.declarativeContent.onPageChanged.addRules([ { conditions : [ new
-	 * chrome.declarativeContent.PageStateMatcher({ pageUrl : { urlMatches:
-	 * '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+' } }) ],
-	 * actions : [ new chrome.declarativeContent.ShowPageAction() ] } ]); });
-	 */
+//Clicking on the button will give the extension access to the current tab,
+// so the icon will not be disabled after the condition below no longer
+// applies.
+// Therefore we have to switch to a more powerful approach to manage the
+// extension state
+/*
+ * chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+ * chrome.declarativeContent.onPageChanged.addRules([ { conditions : [ new
+ * chrome.declarativeContent.PageStateMatcher({ pageUrl : { urlMatches:
+ * '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+' } }) ],
+ * actions : [ new chrome.declarativeContent.ShowPageAction() ] } ]); });
+ */
 
-	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		if(typeof(changeInfo.url) !== 'undefined') {
-			if (regex.test(changeInfo.url)) {
-				chrome.pageAction.show(tabId, function() {
-				});
-			} else {
-				chrome.pageAction.hide(tabId, function() {
-				});
-			}
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(typeof(changeInfo.url) !== 'undefined') {
+		if (regex.test(changeInfo.url)) {
+			chrome.pageAction.show(tabId, function() {
+			});
+		} else {
+			chrome.pageAction.hide(tabId, function() {
+			});
 		}
-	});
+	}
 });
