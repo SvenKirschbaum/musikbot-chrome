49bf3ce65cc97e391a7e1c104d8742873919eddb
Sven Kirschbaum <sven@elite12.de>
2019-02-19 04:22:41 +0100

    Changed back to declarativecontent api

--- Musikbot Chrome Extension/scripts/background.js
+++ Musikbot Chrome Extension/scripts/background.js
@@ -1,8 +1,6 @@
-const regex = /^(http(s)??\:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/;
-
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.get(['authtoken'],function(result) {
-		if(typeof result.authtoken === 'undefined') {
+	chrome.storage.sync.get([ 'authtoken' ], function(result) {
+		if (typeof result.authtoken === 'undefined') {
 			chrome.storage.sync.set({
 				authtoken : ""
 			}, function() {
@@ -10,30 +8,27 @@ chrome.runtime.onInstalled.addListener(function() {
 			});
 		}
 	});
-	
+
 });
 
-//Clicking on the button will give the extension access to the current tab,
+// Clicking on the button will give the extension access to the current tab,
 // so the icon will not be disabled after the condition below no longer
 // applies.
 // Therefore we have to switch to a more powerful approach to manage the
 // extension state
-/*
- * chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
- * chrome.declarativeContent.onPageChanged.addRules([ { conditions : [ new
- * chrome.declarativeContent.PageStateMatcher({ pageUrl : { urlMatches:
- * '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+' } }) ],
- * actions : [ new chrome.declarativeContent.ShowPageAction() ] } ]); });
- */
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+	chrome.declarativeContent.onPageChanged.addRules([ {
+		conditions : [ new chrome.declarativeContent.PageStateMatcher({
+			pageUrl : {
+				urlMatches : '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+'
+			}
+		}) ],
+		actions : [ new chrome.declarativeContent.ShowPageAction() ]
+	} ]);
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(typeof(changeInfo.url) !== 'undefined') {
-		if (regex.test(changeInfo.url)) {
-			chrome.pageAction.show(tabId, function() {
-			});
-		} else {
-			chrome.pageAction.hide(tabId, function() {
-			});
-		}
+	if (typeof (changeInfo.url) !== 'undefined') {
+		chrome.pageAction.hide(tabId,function(){});
 	}
 });
