0d3e40b8f1da863f507925f5b5959f9884b2fb9b
Sven Kirschbaum <sven@elite12.de>
2019-09-23 01:11:16 +0200

    Added functionallity to send Token to Musikbot Site
    #MUS-120

--- Musikbot Chrome Extension/scripts/background.js
+++ Musikbot Chrome Extension/scripts/background.js
@@ -1,13 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.get([ 'authtoken' ], function(result) {
-		if (typeof result.authtoken === 'undefined') {
-			chrome.storage.sync.set({
-				authtoken : ""
-			}, function() {
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.get(['authtoken'], function (result) {
+        if (typeof result.authtoken === 'undefined') {
+            chrome.storage.sync.set({
+                authtoken: ""
+            }, function () {
 
-			});
-		}
-	});
+            });
+        }
+    });
 
 });
 
@@ -16,19 +16,31 @@ chrome.runtime.onInstalled.addListener(function() {
 // applies.
 // Therefore we have to switch to a more powerful approach to manage the
 // extension state
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	chrome.declarativeContent.onPageChanged.addRules([ {
-		conditions : [ new chrome.declarativeContent.PageStateMatcher({
-			pageUrl : {
-				urlMatches : '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+'
-			}
-		}) ],
-		actions : [ new chrome.declarativeContent.ShowPageAction() ]
-	} ]);
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+                urlMatches: '^(http(s)??\\:\\/\\/)?(www\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_])+'
+            }
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (typeof (changeInfo.url) !== 'undefined') {
-		chrome.pageAction.hide(tabId,function(){});
-	}
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (typeof (changeInfo.url) !== 'undefined') {
+        chrome.pageAction.hide(tabId, function () {
+        });
+    }
 });
+
+//React to messages from web page
+chrome.runtime.onMessageExternal.addListener(
+    function (request, sender, sendResponse) {
+    	if(request === "token-request") {
+			chrome.storage.sync.get(['authtoken'],function(result) {
+				sendResponse(result);
+			});
+		}
+    }
+);
